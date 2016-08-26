4603e983de97a62fc0435474d5c017e965066e46
samux1996 <samuel.henrique1996@gmail.com>
2016-08-26 01:06:40 -0300

    :D

---
+++ spider_casas_bahia.py
@@ -0,0 +1,23 @@
+# coding: utf-8 
+import scrapy
+
+
+
+class SpiderCasasBahia(scrapy.Spider):
+    name = 'spider'
+    start_urls = [
+        'http://www.casasbahia.com.br/Informatica/Notebook/?Filtro=C56_C57'
+    ]
+    download_delay = 1.5
+    
+
+    def parse(self, response):
+        for vitrine in response.css('.hproduct'):
+            yield {
+                #'Descrição': vitrine.css('strong::text').extract_first(),
+		'Descrição': vitrine.css('a::attr("title")').extract_first(),
+                'link': vitrine.css('a::attr("href")').extract_first(),
+            }
+	link_next = response.css('ul.ListaPaginas a::attr("href")').extract_first()
+        if link_next:
+            yield scrapy.Request(link_next)
