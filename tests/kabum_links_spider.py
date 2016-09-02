f39e165f211767ce3b86aa1d732844d00674baca
Greg <gregls.2008@gmail.com>
2016-09-02 01:20:46 -0300

    links produtos kabum (sem banco)

--- tests/kabum_links_spider.py
+++ tests/kabum_links_spider.py
@@ -7,7 +7,7 @@ def first(sel, xpath):
 
 class KabumNotebooklLister(scrapy.Spider):
     name='Spider_Kabum'
-    start_urls = ['http://www.kabum.com.br/computadores/notebooks-ultrabooks?ordem=5&limite=100&dep=04&sec=35&cat=&sub=&pagina=1&string=']
+    start_urls = ['http://www.kabum.com.br/computadores/notebooks-ultrabooks']
 
     def parse(self, response):
         for box_page in response.css(".listagem-box"):
@@ -15,4 +15,9 @@ class KabumNotebooklLister(scrapy.Spider):
                 'link': box_page.css('.listagem-titulo_descr a::attr(href)').extract_first(),
                 'description': box_page.css('.listagem-titulo_descr a::text').extract_first(),
             }
+
+        link_next = response.xpath('//*[@id="BlocoConteudo"]/div[2]/div/div[33]/form/table//tr/td[7]/a/@href').extract_first()
+
+        if link_next:
+            yield scrapy.Request('http://www.kabum.com.br/computadores/notebooks-ultrabooks%s'%link_next)
 
