a0a37c70ea0243ad6068f75489362cb75922c2df
Greg <gregls.2008@gmail.com>
2016-09-03 01:13:58 -0300

    Lista Links Kabum

--- tests/kabum_links_spider.py
+++ tests/kabum_links_spider.py
@@ -10,14 +10,12 @@ class KabumNotebooklLister(scrapy.Spider):
     start_urls = ['http://www.kabum.com.br/computadores/notebooks-ultrabooks']
 
     def parse(self, response):
-        for box_page in response.css(".listagem-box"):
-            yield {
-                'link': box_page.css('.listagem-titulo_descr a::attr(href)').extract_first(),
-                'description': box_page.css('.listagem-titulo_descr a::text').extract_first(),
-            }
+        for box_page in response.xpath('//*[@id="BlocoConteudo"]/div[2]/div/div[@class="listagem-box"]/div[@class="listagem-titulo_descr"]/span[@class="H-titulo"]/a[(contains(@href, "/notebook-")) or (contains(@href, "/ultrabook-"))]'):   
+            link = box_page.xpath('./@href').extract()
+            description = box_page.xpath('normalize-space(./text())').extract_first()
+            yield dict(link=link, description=description)
 
         link_next = response.xpath('//*[@id="BlocoConteudo"]/div[2]/div/div[33]/form/table//tr/td[7]/a/@href').extract_first()
 
         if link_next:
             yield scrapy.Request('http://www.kabum.com.br/computadores/notebooks-ultrabooks%s'%link_next)
-
